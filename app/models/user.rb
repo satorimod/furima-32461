230face224fad93bc882eb10679224a1290caecf
satorimod <74944697+satorimod@users.noreply.github.com>
2020-12-23 18:19:27 +0900

    モデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,15 @@ class User < ApplicationRecord
   has_many :items
   has_many :address_items
   
-  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :nickname, presence: true, length: { maximum: 10 }
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password_confirmation,  length: { minimum: 5 }, format: { with: VALID_PASSWORD_REGEX }
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :furigana_last_name, presence: true
+  validates :furigana_first_name, presence: true
+  validates :dob, presence: true
+  
 end
