dcec8415b21bec60455e8b7db038238b80da765e
satorimod <74944697+satorimod@users.noreply.github.com>
2020-12-24 20:09:30 +0900

    ActiveHashモデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
   
+  def create
+    Item.create(item_params)
+  end  
+
   private 
   
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image).merge(user_id: current_user.id)
   end  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,22 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :address_item
   has_one_attached :image
+  belongs_to :category
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+
+    with_options numericality: do
+      validates :price, { greater_than:  300, less_than: 9999999 }
+      
+      with_options { other_than: 1 } do
+        validates :category_id
+        validates :item_state_id
+        validates :shipping_charges_id
+        validates :shipping_area_id
+        validates :shipping_date_id
+        validates :user
+        validates :image
+  end  
 end
