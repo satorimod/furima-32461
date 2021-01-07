ab62ddc079509e8e42d676e236c6fef6f9baab47
satorimod <74944697+satorimod@users.noreply.github.com>
2021-01-07 18:13:36 +0900

    購入画面の表示

--- app/controllers/address_items_controller.rb
+++ app/controllers/address_items_controller.rb
@@ -1,2 +1,22 @@
 class AddressItemsController < ApplicationController
+  def index
+    @address_item = AddressItem.new
+    @item = Item.find(params[:item_id])
+  end
+  
+  def create
+    @address_item = AddressItem.new(address_item_params)
+    if @address_item.valid?
+      @address_item.save
+      redirect_to root_path
+    else
+      render action: :index
+    end  
+  end
+
+  private
+  
+  def address_item_params
+    params.require(:address_item).permit(:user, :item, :postal, :prefecrure_id, :municipality, :address, :building, :phone, :address_item)
+  end  
 end
--- app/models/address_item.rb
+++ app/models/address_item.rb
@@ -1,28 +1,6 @@
-class AddressItem
-  include ActiveModel::Model
-  include ActiveModel::Associations
-  attr_accessr :user, :item, :postal, :prefecrure_id, :municipality, :address, :building, :phone, :address_item
-
+class AddressItem < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :item
-  has_one    :address
-
-  with_options presence: true do
-    validates :postal, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecrure_id, numericality: { other_than: 0, message: "can't be blank" }
-    validates :municipality
-    validates :address
-    validates :phone, format: { with: /\A\d{11}\z/}
-    validates :address_item
-  end
-
-    with_options foreign_key: true do
-      validates :user
-      validates :item
-    end
-
-    def save
-      Address.create(postal: postal, prefecrure_id: prefecrure_id, municipality: municipality, address: address, phone: phone, address_item: address_item, user_id: user_id)
-      AdressItem.create(user: user, item: item)
-    end  
+  has_one    :address_item
 end
